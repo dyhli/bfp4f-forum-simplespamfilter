06444b00da8ddbfdaf3d22c0ab99430f46f439aa
Danny Li <kick_yu_gi_oh@hotmail.com>
2015-01-09 08:48:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 // ==UserScript==
-// @name         BFP4F Simple spam filter
-// @namespace    https://battlefieldtools.com
-// @version      0.12
-// @updateURL    https://raw.githubusercontent.com/dyhli/bfp4f-forum-simplespamfilter/master/script.js
-// @description  A simple SPAM filter for the BFP4F forums, hover to view the original title
-// @author       Danny Li - SharpBunny
-// @include      http://battlefield.play4free.com/en/forum/*
-// @include      http://battlefield.play4free.com/de/forum/*
-// @include      http://battlefield.play4free.com/pl/forum/*
-// @include      http://battlefield.play4free.com/en/forum/*
-// @include      http://battlefield.play4free.com/fr/forum/*
-// @include      http://battlefield.play4free.com/ru/forum/*
-// @grant        none
+// @name			BFP4F Simple spam filter
+// @namespace		https://battlefieldtools.com
+// @version			0.12
+// @updateURL		https://raw.githubusercontent.com/dyhli/bfp4f-forum-simplespamfilter/master/script.js
+// @description		A simple SPAM filter for the BFP4F forums, hover to view the original title
+// @author			Danny Li - SharpBunny
+// @include			http://battlefield.play4free.com/en/forum/*
+// @include			http://battlefield.play4free.com/de/forum/*
+// @include			http://battlefield.play4free.com/pl/forum/*
+// @include			http://battlefield.play4free.com/en/forum/*
+// @include			http://battlefield.play4free.com/fr/forum/*
+// @include			http://battlefield.play4free.com/ru/forum/*
+// @grant			none
 // ==/UserScript==
 
-var spamTitles = ['+91', '91-', '91 -', '@@'];
+/**
+ * YOU CAN EDIT THE THINGS BELOW
+ */
+var spamSettings = {
+	// Add your own SPAM words and whatever
+	spamTitles: ['+91', '91-', '91 -', '@@'],
+	
+	// Do you want to hide it or just replace the title with replaceSpamTitle variable?
+	//   true  = hide
+	//   false = replace title
+	spamHide: false,
+	
+	// Title of the thread if you want to replace it instead
+	replaceSpamTitle: '[SPAM]'
+};
+
+/**
+ * DO NOT EDIT BELOW
+ * DO NOT EDIT BELOW
+ * Unless you know what you're doing...
+ */
 var threadTitles = document.getElementsByClassName('threadtitle');
 
 String.prototype.escapeHTML = function() {
@@ -29,8 +49,8 @@ function checkSpam(str) {
 	if(str.match(/([0-9]{6,15})/g)) {
 		return true;
 	}
-	for(var iv = 0, len = spamTitles.length; iv < len; ++iv) {
-		if(str.indexOf(spamTitles[iv]) != -1) {
+	for(var iv = 0, len = spamSettings.spamTitles.length; iv < len; ++iv) {
+		if(str.indexOf(spamSettings.spamTitles[iv]) != -1) {
 			return true;
 		}
 	}
@@ -48,9 +68,13 @@ for(var i = 0; i < threadTitles.length; i++){
 	var isSpam = checkSpam(titleEl.innerHTML);
 
 	if(isSpam) {
-		titleEl.setAttribute('class', 'subject_old')
-		titleEl.setAttribute('title', titleEl.innerHTML.escapeHTML())
-		titleEl.innerHTML = '[SPAM]';
+		if(spamSettings.spamHide) {
+			titleEl.parentNode.parentNode.parentNode.parentNode.setAttribute('style', 'display:none');
+		} else {
+			titleEl.setAttribute('class', 'subject_old')
+			titleEl.setAttribute('title', titleEl.innerHTML.escapeHTML())
+			titleEl.innerHTML = spamSettings.replaceSpamTitle;
+		}
 	}
 	
 }
